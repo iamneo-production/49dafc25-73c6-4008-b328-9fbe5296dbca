f942cf15a09bc7e4efb677a3fbbbab97df35c82d
Vigneshwar-Raipally <vigneshwarr208@gmail.com>
2023-07-18 16:21:42 +0000

    1

--- social_media_1.sql
+++ social_media_1.sql
@@ -2,4 +2,3 @@ select count(*) from facebook where DOB_YEAR=1999;
 select (count(case when gender='female' then 1 end)/count(*))*100 from facebook;
 select count(*) from facebook where AGE>15;
 select (count(case when gender='female' then 1 end)/count(*))*100 from facebook where AGE<=17;
-select * from facebook;
