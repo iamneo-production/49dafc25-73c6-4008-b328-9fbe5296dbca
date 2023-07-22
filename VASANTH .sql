e37904097fbba340d146bc06978625d8820d6612
vasanthece <vasanth2002raj@gmail.com>
2023-07-22 05:07:23 +0000

    COMPLETED

---
+++ VASANTH .sql
@@ -0,0 +1,19 @@
+select count(*)
+from FACEBOOK
+where dob_year=1999;
+
+select age,count(*)
+from facebook
+group by age 
+order by age;
+
+select count(*) 
+from facebook
+where age>15;
+
+SELECT (SUM(CASE WHEN GENDER = 'female' THEN 1 ELSE 0 END)  / COUNT(*)*100) AS FEMALE_PERCENTAGE
+FROM FACEBOOK;
+
+SELECT (sum(CASE WHEN GENDER = 'female' and AGE>17 THEN 1 ELSE 0 END)  / COUNT(*)*100) AS FEMALE_PERCENTAGE
+FROM FACEBOOK;
+
