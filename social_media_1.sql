d8edf2ea5b4a3a8c0ed49d4ba3d4ce18f1904e98
Vigneshwar-Raipally <vigneshwarr208@gmail.com>
2023-07-25 13:52:35 +0000

    Final_Queries_Facebook_1

--- social_media_1.sql
+++ social_media_1.sql
@@ -1,7 +1,6 @@
 /* 1. Write a SQL Query to find the total number of users who were born in the year 1999? */
 select count(*) from facebook where DOB_YEAR=1999;
 /* 2. Write a SQL Query to count the number of social media users for each age group? */
-select AGE,count(*) from facebook group by AGE;
 select AGE,count(*) from facebook group by AGE order by AGE;
 /* 3. Write a SQL Query to find the total users in social media above age 15? */
 select count(*) from facebook where AGE>15;
@@ -9,11 +8,5 @@ select count(*) from facebook where AGE>15;
 select (count(case when GENDER='female' then 1 end)/count(*))*100 from facebook;
 select (sum(case when GENDER='female' then 1 else 0 end)/count(*)*100) as percentage_of_female_users from facebook;
 /* 5.Write a SQL Query to find the percentage of female users among users above the age 17? */
-select (count(case when GENDER='female' then 1 end)/count(*))*100 from facebook where AGE>17;
-select (sum(case when GENDER='female' then 1 else 0 end)/count(*)*100) from facebook where AGE>17;
-/* Practice Queries */
-select * from facebook;
-select USERID from facebook;
-select AGE from facebook;
-select (count(case when GENDER='female' then 1 end)/count(*)*100) from facebook where AGE<=17;
-select (count(case when GENDER='male' then 1 end)/count(*))*100 from facebook;
+select (count(case when GENDER='female' and age>17 then 1 end)/count(*))*100 from facebook;
+select (sum(case when GENDER='female' and age>17 then 1 else 0 end)/count(*)*100) from facebook;
