cb8b7a8a3bff3871c290d079ecbff7052254c169
Vigneshwar-Raipally <vigneshwarr208@gmail.com>
2023-07-26 09:52:03 +0000

    Backup Commit

--- social_media_1.sql
+++ social_media_1.sql
@@ -13,21 +13,26 @@ select (sum(case when GENDER='female' and age>17 then 1 else 0 end)/count(*)*100
 
 describe plan_table;
 
-/* 1. Creating index for query1 */
+/* 1. Plan explanation of query1 */
 create index facebook_users_born_in_1999_idx on facebook(userid,age,dob_day,dob_year,dob_month,gender,tenure,friend_count,friendships_initiated,likes,likes_received,mobile_likes,mobile_likes_received,www_likes,www_likes_received);
 explain plan for select count(*) from facebook where DOB_YEAR=1999;
 select * from table(dbms_xplan.display());
 
-/* 2. Creating index for query2 */
+/* 2. Plan explanation of query2 */
 create index facebook_age_idx on facebook(AGE);
 explain plan for select AGE,count(*) from facebook group by AGE order by AGE;
 select * from table(dbms_xplan.display());
 
-/* 3. Creating index for query3 */
+/* 3. Plan explanation of query3 */
 explain plan for select count(*) from facebook where age>15;
 select * from table(dbms_xplan.display());
 
-/* 4. Creating index for query4 */
+/* 4. Plan explanation of query4 */
 create index facebook_percentage_of_female_users_idx on facebook(gender);
 explain plan for select(sum(case when GENDER='female' then 1 else 0 end)/count(*)*100) as percentage_of_female_users from facebook;
+select * from table(dbms_xplan.display());
+
+/* 5. Plan explanation of query5 */
+create index facebook_female_users_above_age_17_percentage_idx on facebook(GENDER);
+explain plan for select(sum(case when GENDER='female' and age>17 then 1 else 0 end)/count(*)*100) as percentage_of_female_users from facebook;
 select * from table(dbms_xplan.display());
