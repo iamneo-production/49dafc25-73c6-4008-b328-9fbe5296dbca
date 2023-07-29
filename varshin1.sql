9c68ef685c433929112536c29dfa094a76d93c29
varshin1 <varshiniv238@gmail.com>
2023-07-29 09:41:27 +0000

    Commit of all the queries

--- varshin1.sql
+++ varshin1.sql
@@ -7,8 +7,8 @@ select age, count(*) from FACEBOOK group by age order by age;
 --Sql query to find the total users in social media whoose age is above 15
 select count(*) from FACEBOOK where age>15;
 
---Sql query to find the percentage of the female users who are using social media
+--Sql query to find the percentage of female users who are using social media
 select(sum(case when gender = 'female' then 1 else 0 end) / count(*)*100)as female_percentage from FACEBOOK;
 
---Sql query to find the percentage of the female users among users above the age of 17
+--Sql query to find the percentage of female users among users above the age of 17
 select(sum(case when gender = 'female' and age>17 then 1 else 0 end) / count(*)*100) as female_percentage from FACEBOOK;
