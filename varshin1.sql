5fd27fad3daf18811534ba6f50d30eec3209d37a
varshin1 <varshiniv238@gmail.com>
2023-07-29 14:40:46 +0000

    REMOVED SPACE

--- varshin1.sql
+++ varshin1.sql
@@ -7,7 +7,7 @@ select age, count(*) from FACEBOOK group by age order by age;
 --Sql query to find the total users in social media whoose age is above 15
 select count(*) from FACEBOOK where age>15;
 
---Sql query to find the percentage of  female users who are using social media
+--Sql query to find the percentage of female users who are using social media
 select(sum(case when gender = 'female' then 1 else 0 end) / count(*)*100)as female_percentage from FACEBOOK;
 
 --Sql query to find the percentage of female users among users above the age of 17
