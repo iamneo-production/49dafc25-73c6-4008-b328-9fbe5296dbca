34cf4f46ca18ebb582ecdf39778ba768dbf58701
varshinigadiraju <varshinigadiraju58@gmail.com>
2023-07-30 14:47:20 +0000

    hey

--- varshinigadiraju.sql
+++ varshinigadiraju.sql
@@ -16,7 +16,7 @@ where age>15;
 select (sum(case when gender='female' then 1 else 0 end)/count(*)*100)as FEMALE_PERCENTAGE
 from facebook;
 
---5.Write a SQL Query to find the percentage of female users among users above the age of 17
+--5.Write a SQL Query to find the percentage of female users among users above the age of 17 
 select (sum(case when gender='female' and age>17 then 1 else 0 end)/count(*)*100) as FEMALE_PERCENTAGE
 from facebook;
 
