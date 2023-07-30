3079f91c2815bc47b99758fa881064fd0fbb096b
VenkataSaiRajeshmarrey <rajeshalwa33@gmail.com>
2023-07-30 12:39:17 +0000

    recent submit

--- virtusa project.sql
+++ virtusa project.sql
@@ -3,18 +3,16 @@ select count(*) from facebook where dob_year=1999;
 
 --no of users for each age group
 select count(*) from facebook;
-select age,count(*) from facebook group by age order by age;
+select age,count(*) from facebook group by age;
 
 --total users above age 15
 select count(*) from facebook where age>15;
 
 --percentage of female users
-select count(*) from facebook where gender='female';
 select (sum(case when gender='female' then 1 else 0 end)/count(*))*100
 as per_female from facebook;
 
 --percentage of female users above age 17 years
-select count(*) from facebook where gender='female' and age>17;
 select (sum(case when gender='female' and age>17 then 1 else 0 end)/count(*))*100
 as per_female from facebook;
 
